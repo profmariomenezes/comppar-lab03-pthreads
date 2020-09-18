6186a4c7ccb3fb83b3edaf03c9c407f0cbebdc32
Mario O. de Menezes <58959503+mariomack@users.noreply.github.com>
2020-09-18 19:28:57 -0300

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int fib(int n)                             
+ {                                          
+ /* funcao que calcula o fibonacci */
+ }                                          
+  
+ int main(void)
+  {
+   int n;
+   scanf("%d", &n);
+   printf("%d\n", fib(n));
+   return 0;
+  }
